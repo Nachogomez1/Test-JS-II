9d1fccaf87098531ed0747ac1b2169808972b3e1
Ignacio Gomez <ignacionicog@gmail.com>
2022-08-19 19:49:35 -0300

    subo ejercicios repositorio 2 js

--- JavaScript-4/tareas/tareas.js
+++ JavaScript-4/tareas/tareas.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  let gatito = {};
+  gatito.nombre = nombre,    
+  gatito.edad = edad,
+  gatito.meow = function(){
+      return "Meow!";
+    }
+  return gatito;
+
 }
 
 function agregarPropiedad(objeto, propiedad) {
@@ -13,6 +21,8 @@ function agregarPropiedad(objeto, propiedad) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "propiedad" (una cadena/string)
   // Tu código:
+  objeto[propiedad] = null;
+  return objeto
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -20,12 +30,15 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // No necesita un 'return'
   // Tu código:
+  objeto [ metodo ]();
+
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
+  return objetoMisterioso.numeroMisterioso * 5 ;
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
@@ -33,25 +46,45 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto [unaPropiedad];
+  return objeto;
+
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  let usuarioNuevo={}
+  usuarioNuevo.nombre = nombre,
+  usuarioNuevo.email = email,
+  usuarioNuevo.password = password;
+  return usuarioNuevo
+
 }
 
 function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
-}
+  if(usuario.email){
+    return true
+  }
+  return false
+  }
 
 function tienePropiedad(objeto, propiedad) {
   // Devuelve "true" si el objeto tiene el valor del argumento "propiedad"
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(objeto [propiedad]==true){
+    return true
+  }
+  else{
+    return false
+  }
+
 }
 
 function verificarPassword(usuario, password) {
@@ -59,12 +92,16 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  return usuario.password===password
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password=nuevaPassword
+  return usuario
+
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -72,6 +109,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigos.push(nuevoAmigo)
+  return usuario
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -80,7 +119,12 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  usuarios.forEach((elemento)=> {
+    elemento.esPremium = true
+})
+return usuarios;
 }
+
 
 function sumarLikesDeUsuario(usuario) {
   // "usuario" tiene una propiedad llamada "posts" que es un array
@@ -89,7 +133,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  let suma = 0;
+  usuario.posts.forEach((persona)=>{suma =suma + persona.likes})
+  return suma ;
 }
+
 
 function agregarMetodoCalculoDescuento(producto) {
   // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
@@ -101,7 +149,14 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  producto.calcularPrecioDescuento = function(){
+    let descuento;
+    descuento = this.precio * this.porcentajeDeDescuento;
+    return this.precio - descuento;
+  }
+  return producto;
 }
+  
 
 // No modificar nada debajo de esta línea, sino no correrán los test!
 // --------------------------------
--- JavaScript-5/tareas/tareas.js
+++ JavaScript-5/tareas/tareas.js
@@ -8,12 +8,26 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  return class Usuario {
+    constructor(opciones){
+      this.usuario = opciones["usuario"];
+      this.nombre = opciones["nombre"];
+      this.email = opciones["email"];
+      this.password = opciones ["password"];
+    }
+    saludar (){
+      return `Hola, mi nombre es ${this.nombre}`;
+    }
+  };
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function (){
+    return 'Hello World!';
+  }
 }
 
 function agregarStringInvertida() {
@@ -22,6 +36,15 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'alumno'.reverse() => 'onmula'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+  String.prototype.reverse = function () {
+    let str = this;
+    arrayStr = str.split('');
+    let reverseArray = [];
+    for (let i= 1; i <= arrayStr.length; i++){
+      reverseArray.push(arrayStr[arrayStr.length - i]);
+    }
+    return reverseArray.join('');
+  }
 }
 
 // ---------------------------------------------------------------------------//
@@ -36,9 +59,15 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
-      // Crea el constructor:
+    constructor(nombre , apellido, edad, domicilio) {
+      this.nombre = nombre ;
+      this.apellido = apellido ;
+      this.edad = edad ;
+      this.domicilio = domicilio ;
 
+    }
+    detalle(){
+      return `Nombre ${this.nombre} Apellido ${this.apellido} Edad ${this.edad} Domicilio${this.domicilio}`
     }
 }
 
@@ -46,11 +75,15 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  const nuevaPersona = new Persona("Juan","Perez", 22, "Saavedra 123");
+  return nuevaPersona;
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  return Persona.prototype.datos = function() {
+    return `${this.nombre}, ${this.edad} años`};
 }
   
 
